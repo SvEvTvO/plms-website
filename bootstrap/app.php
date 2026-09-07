01a7709fa1eed08d875a126bda3b8f54a3eb0622
Seto <sultanagungper@gmail.com>
2026-09-07 11:34:53 +0700

    chore: setup vercel serverless configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,19 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
-        );
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
     })->create();
+
+// --- TAMBAHKAN KODE INI UNTUK VERCEL ---
+if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
+    $app->useStoragePath('/tmp/storage');
+}
+// ---------------------------------------
+
+return $app;
