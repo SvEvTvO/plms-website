49c691369ce91c2cc6a460d0de4952a3c69078f6
Seto <sultanagungper@gmail.com>
2026-09-08 10:04:36 +0700

    fixing 419 bug fkin shit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // KUNCI ANTI-419: Paksa HTTPS di Vercel agar CSRF Token tidak diblokir browser
-        if (config('app.env') === 'production' || isset($_SERVER['VERCEL']) || env('IS_VERCEL', true)) {
+        if (config('app.env') === 'production' || isset($_SERVER['VERCEL']) || env('IS_VERCEL', false)) {
             URL::forceScheme('https');
         }
     }
